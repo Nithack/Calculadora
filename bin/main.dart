cd26b3a2aa76b401a5fc9ab7987da5b0905d7934
Andrey <andreynithack@gmail.com>
2020-02-24 14:43:16 -0300

    Arquivo main com os comandos de teste

---
+++ bin/main.dart
@@ -0,0 +1,10 @@
+
+import 'calculadora.dart';
+
+void main(List<String> arguments) {
+  print(calcular("+", 1, 2));
+  print(calcular("-", 1, 2));
+  print(calcular("*", 1, 2));
+  print(calcular("/", 1, 2));
+  print(calcular("a", 1, 2));
+}
